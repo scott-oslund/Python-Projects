a3ed18242cc94076b7e4ba099ad4e5f4aa8fa21e
Scott Oslund <sroslund@ucsc.edu>
2020-09-19 16:08:53 -0700

    added card class and deck methods for initing the deck

--- Blackjack/deck.py
+++ Blackjack/deck.py
@@ -1,7 +1,15 @@
 class deck:
 
     def __init__(self):
-        pass
+        self.cards = []
+        self.reset()
+
+    def reset(self):
+        suits = ["spades", "clubs", "diamonds", "hearts"]
+        for suit in suits:
+            for i in range(14): # 0 = ace, 11 = jack ... 13 = king
+                self.cards.append(card(i,suit))
+
 
     def shuffle(self):
         pass
@@ -11,3 +19,21 @@ class deck:
 
     def take(self):
         pass
+
+    def print(self):
+        for card in self.cards:
+            print(card)
+
+
+class card:
+
+    def __init__(self, num, suit):
+        self.num = num
+        self.suit = suit
+
+    def __repr__(self):
+        return "%s___%d" % (self.suit, self.num)
+
+if __name__ == "__main__":
+    deck1 = deck()
+    deck1.print()
